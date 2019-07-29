e3b957f7bf311e345d94e87c1fd89e83568dac9a
jubeen12 <jubeen12@gmail.com>
2019-07-29 09:43:02 +0530

    Updates

---
+++ precache-manifest.306119af7d8958e27f567c2d8bcb39d2.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "7e5fcba001ff19148f55",
+    "url": "/redux-reg-login-fb-bootsrap/static/css/main.bd61b9b9.chunk.css"
+  },
+  {
+    "revision": "4d5ead9ac4517cea0fb3",
+    "url": "/redux-reg-login-fb-bootsrap/static/js/runtime~main.d4ccd679.js"
+  },
+  {
+    "revision": "6dca6c52621849cf702a",
+    "url": "/redux-reg-login-fb-bootsrap/static/js/2.d09e30ea.chunk.js"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/team-4-800x800.23007132.jpg"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "ab904daa548967670847fc3929bf50f4",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/bootstrap.ab904daa.jpg"
+  },
+  {
+    "revision": "368685db949cddcd901ba54559a727d9",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/angular.368685db.jpg"
+  },
+  {
+    "revision": "e7c82e5c569db9d9761b13ef1384f702",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/sketch.e7c82e5c.jpg"
+  },
+  {
+    "revision": "c4eb3d0be0cf5a2d156a123bdf7fb40d",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/react.c4eb3d0b.jpg"
+  },
+  {
+    "revision": "9378e368a757bd0c7ca556a0258cb527",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/vue.9378e368.jpg"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "7e5fcba001ff19148f55",
+    "url": "/redux-reg-login-fb-bootsrap/static/js/main.3199e380.chunk.js"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "e2582654b8e7b6c285d5017df6427d73",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/profile-cover.e2582654.jpg"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/redux-reg-login-fb-bootsrap/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "74efe14b2d1f1dc900d725d7006e6f32",
+    "url": "/redux-reg-login-fb-bootsrap/index.html"
+  }
+];
